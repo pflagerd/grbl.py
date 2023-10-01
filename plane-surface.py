7e2ebdbd7c5cc51890cefeb50d2df23983faeb91
Daniel P Pflager <daniel.pflager@gmail.com>
2023-09-30 22:04:11 -0700

    Errors

--- plane-surface.py
+++ plane-surface.py
@@ -20,7 +20,7 @@ def main(args):
     cutter = type('', (), {})()
     cutter.diameter = 25  # 25 mm diameter
     cutter.overlapPercentage = 25  # 25 percent of diameter
-    cutter.swatheWidth = cutter.diameter - cutter.diameter * cutter.overlapPercentage
+    cutter.swatheWidth = cutter.diameter - cutter.diameter * cutter.overlapPercentage  # width of cutter movement in y direction
     cutter.x = 0
     cutter.y = 0
     cutter.z = 0
@@ -30,13 +30,13 @@ def main(args):
     # except that we want to always end on a standard cut (not a climb cut), which means we want an odd number of passes
     if numberOfPasses % 2 == 0:
         numberOfPasses += 1
+    cutter.swatheWidth = workPieceSize.y / numberOfPasses  # adjust to match odd number of passes
 
     moveInAStraightLine(cutter.x, cutter.y, cutter.z + 1, 500)  # fairly quick move to 1 mm above workpiece
 
     facingDepth = 0.5  # 0.5mm cutting depth
     cutter.z -= facingDepth
-
-    cutter.swatheWidth = workPieceSize.y / numberOfPasses
+    moveInAStraightLine(cutter.x, cutter.y, cutter.z, 100)  # cut slowly down into workpiece
 
     for passNumber in range(numberOfPasses):
         if passNumber % 2 == 0:  # passNumber counts from zero, so even passes are left-to-right
@@ -46,7 +46,7 @@ def main(args):
         moveInAStraightLine(cutter.x, cutter.y, cutter.z, 750)
         if passNumber != numberOfPasses - 1:
             cutter.y += cutter.swatheWidth
-            moveInAStraightLine(cutter.x, cutter.y, cutter.z, 250)
+            moveInAStraightLine(cutter.x, cutter.y, cutter.z, 375)  # move slower in Y direction
 
     cutter.z = clearanceHeight    # raise the cutting tool to 10mm above the workpiece's original size
     moveInAStraightLineRapidly(cutter.x, cutter.y, cutter.z)
