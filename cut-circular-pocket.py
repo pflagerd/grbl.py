13acc2a0092c6b77c2a73fe0397877bc77e57b01
Daniel P Pflager <daniel.pflager@gmail.com>
2023-10-11 17:05:42 -0700

    increase speed.

--- cut-circular-pocket.py
+++ cut-circular-pocket.py
@@ -1,6 +1,8 @@
 #!/usr/env python
 from grbl import *
 from math import *
+from datetime import *
+
 
 def main(args):
     initializeGrbl()
@@ -13,8 +15,8 @@ def main(args):
     printGrblStatus()
 
     dustShoeClearanceHeight = 30
-    cuttingSpeed = 200
-    plungeSpeed = 50
+    cuttingSpeed = 300
+    plungeSpeed = 20
     plungeDepth = 1
     depth = 10
     radius = 30.5
@@ -30,7 +32,7 @@ def main(args):
 
     cutter = type('', (), {})()
     cutter.radius = 11.8  # 23.6 mm diameter
-    cutter.overlapPercentage = 33  # percent of cutter.radius
+    cutter.overlapPercentage = 66  # percent of cutter.radius
     cutter.swatheWidth = cutter.radius - cutter.radius * cutter.overlapPercentage / 100  # width of cutter movement in y direction
     cutter.position = type('', (), {})()
     cutter.position.x = 0
@@ -44,32 +46,39 @@ def main(args):
     input("Put dust shoe on cutter.  Hit enter when ready. ")
 
     while True:
+        startTime = datetime.now()
         startSpindle(10000)  # start spindle
 
         cutter.position.x = cutter.position.y = cutter.position.z = 0
         moveInAStraightLineRapidly(cutter.position.x, cutter.position.y, cutter.position.z + 1)
 
+        firstCut = True
         while cutter.position.z > -depth:
             cutRadius = cutter.radius
             cutter.position.z = max(cutter.position.z - plungeDepth, -depth)
-            moveInAStraightLine(cutter.position.x, cutter.position.y, cutter.position.z, plungeSpeed)
+            # moveInAStraightLine(cutter.position.x, cutter.position.y, cutter.position.z, plungeSpeed)
 
             lastCutRadius = cutRadius
             while cutRadius < radius:
                 cutRadius = min(cutRadius + cutter.swatheWidth, radius)
 
                 cutter.position.x = (cutRadius - cutter.radius)
-                moveInAnArcClockwise(cutter.position.x, cutter.position.y, cutter.position.z, (cutRadius - lastCutRadius) / 2, 0, cuttingSpeed)
-                moveInAnArcClockwise(cutter.position.x, cutter.position.y, cutter.position.z, -(cutRadius - cutter.radius), 0, cuttingSpeed)
+                moveInAnArcClockwise(cutter.position.x, cutter.position.y, cutter.position.z, (cutRadius - lastCutRadius) / 2, 0, plungeSpeed if firstCut else cuttingSpeed / 2)
+                moveInAnArcClockwise(cutter.position.x, cutter.position.y, cutter.position.z, -(cutRadius - cutter.radius), 0, cuttingSpeed / 5 if firstCut else cuttingSpeed)
+
+                firstCut = False
                 lastCutRadius = cutRadius
 
+            moveInAnArcClockwise(cutter.position.x, cutter.position.y, cutter.position.z, -(cutRadius - cutter.radius),
+                                 0, cuttingSpeed)
             moveInAStraightLineRapidly(cutter.position.x, cutter.position.y, cutter.position.z + 1)
             cutter.position.x = cutter.position.y = 0
-            moveInAStraightLineRapidly(cutter.position.x, cutter.position.y, cutter.position.z + 1)
+            moveInAStraightLineRapidly(cutter.position.x, cutter.position.y, cutter.position.z)
 
         moveInAStraightLineRapidly(0, 0, 0)  # raise the cutting tool to its starting position.
         moveInAStraightLineRapidly(0, 0, dustShoeClearanceHeight)  # raise the cutting tool to its starting position.
         stopSpindle()
+        print("Elapsed time " + str(datetime.now() - startTime))
 
         response = input("Do another pass? y/N ")
         if response not in ["y", "Y"]:
