2d46cd83190c87f3539bc7ccb3ddec798c5991b8
Daniel P Pflager <daniel.pflager@gmail.com>
2024-10-05 07:45:28 -0700

    Ten runs of lower left homing.

--- probe_machine_coordinates_of_all_limits.py
+++ probe_machine_coordinates_of_all_limits.py
@@ -8,8 +8,16 @@ from find_and_set_home import bottom_left_z_up
 from find_and_set_home import top_right_z_down
 
 if __name__ == '__main__':
-    find_and_set_home(top_right_z_up)
-    find_and_set_home(top_right_z_down)
-    find_and_set_home(top_left_z_up)
-    find_and_set_home(bottom_right_z_up)
-    find_and_set_home(bottom_left_z_up)
+    user_input = input("WARNING: Please remove any tool from the spindle.  This will probe for the minimum z-coordinate, which will potentially plunge a tool into the spoiler-board. ")
+    #print("You entered:", user_input)
+
+    top_right_up = find_and_set_home(top_right_z_up)
+    top_right_down = find_and_set_home(top_right_z_down)
+    top_left_up = find_and_set_home(top_left_z_up)
+    bottom_right_up = find_and_set_home(bottom_right_z_up)
+    bottom_left_up = find_and_set_home(bottom_left_z_up)
+    print(F'top_right_up == {top_right_up}')
+    print(F'top_right_down == {top_right_down}')
+    print(F'top_left_up == {top_left_up}')
+    print(F'bottom_right_up == {bottom_right_up}')
+    print(F'bottom_left_up == {bottom_left_up}')
