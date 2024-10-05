9b32eefcfa755cd579956fd6cd0a46f3a872b1ee
Daniel P Pflager <daniel.pflager@gmail.com>
2024-10-05 15:46:59 -0700

    Created new GrblController class.  Starting to move logic into it.

--- home_and_move_to_default_lower_left_origin.py
+++
@@ -1,4 +0,0 @@
-
-lower_left_origin_machine_coordinates = (-405.000, -299.000, -84.000)
-
-if __name__ == '__main__':
--- probe_machine_coordinates_of_all_limits.py
+++ probe_machine_coordinates_of_all_limits.py
@@ -1,21 +1,21 @@
 #!/usr/env python
 
-from find_and_set_home import find_and_set_home
-from find_and_set_home import top_right_z_up
-from find_and_set_home import top_left_z_up
-from find_and_set_home import bottom_right_z_up
-from find_and_set_home import bottom_left_z_up
-from find_and_set_home import top_right_z_down
+from find_home_and_set_origin import find_home_and_set_origin
+from find_home_and_set_origin import top_right_z_up
+from find_home_and_set_origin import top_left_z_up
+from find_home_and_set_origin import bottom_right_z_up
+from find_home_and_set_origin import bottom_left_z_up
+from find_home_and_set_origin import top_right_z_down
 
 if __name__ == '__main__':
     user_input = input("WARNING: Please remove any tool from the spindle.  This will probe for the minimum z-coordinate, which will potentially plunge a tool into the spoiler-board. ")
     #print("You entered:", user_input)
 
-    top_right_up = find_and_set_home(top_right_z_up)
-    top_right_down = find_and_set_home(top_right_z_down)
-    top_left_up = find_and_set_home(top_left_z_up)
-    bottom_right_up = find_and_set_home(bottom_right_z_up)
-    bottom_left_up = find_and_set_home(bottom_left_z_up)
+    top_right_up = find_home_and_set_origin(top_right_z_up)
+    top_right_down = find_home_and_set_origin(top_right_z_down)
+    top_left_up = find_home_and_set_origin(top_left_z_up)
+    bottom_right_up = find_home_and_set_origin(bottom_right_z_up)
+    bottom_left_up = find_home_and_set_origin(bottom_left_z_up)
     print(F'top_right_up == {top_right_up}')
     print(F'top_right_down == {top_right_down}')
     print(F'top_left_up == {top_left_up}')
