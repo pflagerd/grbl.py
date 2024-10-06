34a25117a9ce70081006c9c286c8c386f39b8e0d
Daniel P Pflager <daniel.pflager@gmail.com>
2024-10-06 11:35:08 -0700

    Print the final machine coordinates

--- run_homing_cycle_and_move_to_default_lower_left_origin.py
+++ run_homing_cycle_and_move_to_default_lower_left_origin.py
@@ -5,4 +5,4 @@ lower_left_origin_machine_coordinates = (-405.000, -299.000, -84.000)
 if __name__ == '__main__':
     grblController = GrblController()
     grblController.runHomingCycle()
-    grblController.moveToMachineCoordinates(*lower_left_origin_machine_coordinates)
+    print(grblController.moveToMachineCoordinates(*lower_left_origin_machine_coordinates))
