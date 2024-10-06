e14a1371bd29610e3ccdcc56783aec4452cfe7d2
Daniel P Pflager <daniel.pflager@gmail.com>
2024-10-05 17:17:03 -0700

    moveToMachineCoordinates()

--- GrblController.py
+++ GrblController.py
@@ -13,12 +13,12 @@ class GrblController(serial.Serial):
         topRightZDown = b'4'
 
     def __init__(self, portDeviceName="/dev/ttyUSB0", portBaudRate=115200):
-
         self.homingPosition = self.HomingPositions.bottomLeftZUp
+        self.machineCoordinates = None
 
         self.portDeviceName = portDeviceName
         self.portBaudRate = portBaudRate
-        super().__init__(self.portDeviceName, self.portBaudRate)
+        super().__init__(self.portDeviceName, self.portBaudRate, timeout=30)
 
         line = super().readline()
         print(str(line))
@@ -36,9 +36,58 @@ class GrblController(serial.Serial):
             print("Unexpected response " + str(line))
             sys.exit(1)
 
+        self.getMachineCoordinates()
+
     def __del__(self):
-        if super():
+        if super():  # TODO: ChatGPT suggested super().close() and it crashed so I added the if.  I don't about this.
             super().close()
+
+    def getMachineCoordinates(self):
+        # get current machine coordinates
+        print("sending ?\\n")  # Status report query.
+        super().write(b'?\n')
+        while True:
+            line = super().readline()
+            print(str(line))
+            # b'<Idle|MPos:-417.000,-307.000,-3.000|Bf:15,127|FS:0,0|WCO:-417.000,-307.000,-3.000>\r\n'
+            if b'MPos' in line:
+                statusLines = line.decode('utf-8').split('|')
+                indexOfColon = statusLines[1].index(':')
+                machineCoordinates = statusLines[1][indexOfColon + 1:].split(',')
+                print('machineCoordinates == ', machineCoordinates)
+            if line == b"ok\r\n":
+                break
+        self.machineCoordinates = machineCoordinates
+        return machineCoordinates
+
+    def moveToMachineCoordinates(self, x=None, y=None, z=None, feedRate=400):
+        machineCoordinates = self.getMachineCoordinates()
+
+        if x is None and y is None and z in None:
+            print("At least one of x, y and z must be set")
+            return machineCoordinates
+
+        gcode = f"G90 G53 G0 F{feedRate}"
+        if x is not None:
+            gcode += f" X{x}"
+        if y is not None:
+            gcode += f" Y{y}"
+        if x is not None:
+            gcode += f" Z{z}"
+
+        print(gcode)
+        super().write(gcode.encode('utf-8'))
+        while True:
+            line = super().readline()
+            print(str(line))
+            if line in [b"ok\r\n", b""]:
+                break
+
+        if line != b"ok\r\n":
+            return None  # TODO: DPP: Maybe I should return the current machine coordinates, since I won't be moving.
+
+        # get current machine coordinates
+        return self.getMachineCoordinates()
 
     # See: https://github.com/gnea/grbl/wiki/Grbl-v1.1-Commands#:~:text=run%20as%20normal.-,%24H%20%2D%20Run%20homing%20cycle,-This%20command%20is
     def runHomingCycle(self, homingPosition=HomingPositions.bottomLeftZUp):
