c7fb3fe395f92534fef08ad4d7462d6a46b869ec
Daniel P Pflager <daniel.pflager@gmail.com>
2024-10-28 17:57:27 -0700

    rationalize naming

--- cut_vertical_lines.py
+++ cut_vertical_lines.py
@@ -1,40 +1,48 @@
 from GrblController import *
 
-lower_left_origin_machine_coordinates = (-405.000, -297.000, -68.7)
+workPieceLowerLeftOriginMachineCoordinates = (-405.000, -297.000, -68.7)
+
+
+def inclusiveFloatRange(start, stop, step):
+    while start < stop + step:
+        yield start  # rounding to avoid floating-point precision issues
+        start += step
+
 
 if __name__ == "__main__":
     grblController = GrblController()
     print('machine coordinates at home == ' + str(grblController.runHomingCycle()))
 
-    betweenBlocks = 30
-    betweenLines = 5
-    blockWidth = 30
-    depth = 5.1
+    blockWidth = 20
+    currentBlockY = 0
+    currentLineY = 0
+    depth = 5
     feedRate = 1000
-    left = 10
-    length = 30
-    top = 50
-    workPieceWidth = 360
+    lineHeight = 30
+    safeZAboveZOrigin = 5
+    widthBetweenLines = 5
+    widthBetweenBlocks = 30
+    workPieceMaximumX = 390
+    workPieceMaximumY = 290
+    workPieceMinimumX = 10
+    workPieceMinimumY = 10
 
-    grblController.moveToMachineCoordinates(z=lower_left_origin_machine_coordinates[2] + 5)
-    for l in range(depthIncrement, 2.1,  1 / 2)
-        for k in range(left, betweenBlocks, workPieceWidth):
-            for j in range(left, betweenLines, blockWidth):
-                for i in range(0, depth, l):
-                    grblController.moveToMachineCoordinates(x=lower_left_origin_machine_coordinates[0] + left + j + k,
-                                                            y=lower_left_origin_machine_coordinates[1] + top)
+    grblController.moveToMachineCoordinates(z=workPieceLowerLeftOriginMachineCoordinates[2] + safeZAboveZOrigin)
+    for currentZIncrement in inclusiveFloatRange(1 / 2, 3, 1 / 2):  # depth increments
+        for currentBlockX in inclusiveFloatRange(0, widthBetweenBlocks, workPieceMaximumX - blockWidth - workPieceMinimumX):
+            for currentLineX in inclusiveFloatRange(0, blockWidth, widthBetweenLines):
+                for currentLineZ in inclusiveFloatRange(0, depth, currentZIncrement):
+                    grblController.moveToMachineCoordinates(x=workPieceLowerLeftOriginMachineCoordinates[0] + workPieceMinimumX + currentBlockX + currentLineX,
+                                                            y=workPieceLowerLeftOriginMachineCoordinates[1] + workPieceMinimumY + currentBlockY + currentLineY + lineHeight)
 
-                    grblController.cutToMachineCoordinates(z=lower_left_origin_machine_coordinates[2] - i,
+                    grblController.cutToMachineCoordinates(z=workPieceLowerLeftOriginMachineCoordinates[2] - currentLineZ,
                                                            feedRate=feedRate / 2)
 
-                    grblController.cutToMachineCoordinates(x=lower_left_origin_machine_coordinates[0] + left + j + k,
-                                                           y=lower_left_origin_machine_coordinates[1] + top - length,
+                    grblController.cutToMachineCoordinates(x=workPieceLowerLeftOriginMachineCoordinates[0] + workPieceMinimumX + currentBlockX + currentLineX,
+                                                           y=workPieceLowerLeftOriginMachineCoordinates[1] + workPieceMinimumY + currentBlockY + currentLineY,
                                                            feedRate=feedRate)
-                    grblController.moveToMachineCoordinates(z=lower_left_origin_machine_coordinates[2] + 5)
+                    grblController.moveToMachineCoordinates(z=workPieceLowerLeftOriginMachineCoordinates[2] + safeZAboveZOrigin)
 
-    grblController.moveToMachineCoordinates(lower_left_origin_machine_coordinates[0],
-                                            lower_left_origin_machine_coordinates[1] + 280,
+    grblController.moveToMachineCoordinates(workPieceLowerLeftOriginMachineCoordinates[0],
+                                            workPieceLowerLeftOriginMachineCoordinates[1] + 280,
                                             -5)
-
-
-
