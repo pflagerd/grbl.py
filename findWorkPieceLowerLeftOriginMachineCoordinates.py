c010e3e84b8d435d4d628286c2d4fb71b274ae0b
Daniel P Pflager <daniel.pflager@gmail.com>
2025-04-10 12:53:05 -0700

    Not a great name

--- findWorkPieceLowerLeftOriginMachineCoordinates.py
+++ findWorkPieceLowerLeftOriginMachineCoordinates.py
@@ -1,6 +1,7 @@
 from GrblController import *
 
-workPieceLowerLeftOriginMachineCoordinates = GrblController.Vector(-405.000, -297.000, -81.0)
+#workPieceLowerLeftOriginMachineCoordinates = GrblController.Vector(-405.000, -297.000, -81.0)
+workPieceLowerLeftOriginMachineCoordinates = GrblController.Vector(-407.000, -300.000, -89.0)   # -89
 
 if __name__ == '__main__':
     grblController = GrblController()
