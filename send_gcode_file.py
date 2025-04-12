871fc582ed0c4d7f565b5e0e540d7d277fafa2a4
Daniel P Pflager <daniel.pflager@gmail.com>
2025-04-11 21:02:33 -0700

    Find depth with 2mm bit and cut.

--- send_gcode_file.py
+++ send_gcode_file.py
@@ -2,15 +2,18 @@ from datetime import datetime
 
 from GrblController import *
 
-workPieceLowerLeftOriginMachineCoordinates = GrblController.Vector(-405.000, -297.000, -81.0)
+workPieceLowerLeftOriginMachineCoordinates = GrblController.Vector(-409.000, -298.000, -70)
 
 if __name__ == "__main__":
     safeZAboveZOrigin = 5
 
     grblController = GrblController()
     print(grblController.runHomingCycle())
+    print(grblController.moveToMachineCoordinates(*workPieceLowerLeftOriginMachineCoordinates))
+    print(grblController.setOrigin(*workPieceLowerLeftOriginMachineCoordinates))
 
     start = datetime.now()
-    grblController.sendFile("/home/oy753c/desktops/neon-candle/carveco/Toolpaths/Toadstool Logo Scaled to 30 wide - toad-ol.birch plywood.B/toadol roundover 0-12.5 f200 cw.gcode")
+    # grblController.sendFile("/home/oy753c/desktops/neon-candle/carveco/Toolpaths/Toadstool Logo Scaled to 30 wide - sto.birch plywood.B/lower left sto climb inside.transformed.gcode")
+    grblController.sendFile("/home/oy753c/desktops/neon-candle/carveco/Toolpaths/Toadstool Logo Scaled to 30 wide - sto.birch plywood.B/cutToadstoolStoLowerLeft.gcode")
     finish = datetime.now()
     print(f'Elapsed time was {finish - start}')
