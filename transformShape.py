bf750ab7d38813f93f434935db44beb5eafcc763
Daniel P Pflager <daniel.pflager@gmail.com>
2025-04-11 20:17:28 -0700

    Create a cutting path for the same sto shape.

--- transformShape.py
+++ transformShape.py
@@ -80,6 +80,13 @@ def transformGcode(gcodeInputLines, XOffset=0.0, YOffset=0.0, ZDepth=-1.0, ZFeed
 
 
 def main(args):
+    return cutToadstoolStoLowerLeft()
+
+    #gcodeOutputLines = transformShapeString(gcodeInputLines, -0.2046, 160.6439)
+    #gcodeOutputLines = transformShapeString(lowerLeftStoClimbInsideGcode, 179.862, 90.6404)
+
+
+def carveToadstoolStoLowerLeft():
     lowerLeftStoConventionalInsideGcodeFilename = "/home/oy753c/desktops/toadstool/carveco/Toolpaths/Toadstool Logo Scaled to 30 wide - sto.birch plywood.B/lower left sto conventional inside.gcode"
     with open(lowerLeftStoConventionalInsideGcodeFilename, "r") as gcodeFile:
         lowerLeftStoConventionalInsideGcode = gcodeFile.read()
@@ -143,9 +150,64 @@ def main(args):
     gcodeOutputLines += "G0X0.0000Y290.0000\n"  # Rapidly move the cutting head to top left out-of-the-way position
     gcodeOutputLines += "M2\n"                  # End program.
 
-    outputFileName = lowerLeftStoClimbInsideGcodeFilename.replace(".gcode", ".transformed.gcode")
-    with open(outputFileName, "w") as gcodeFile:
+    with open("/home/oy753c/desktops/toadstool/carveco/Toolpaths/Toadstool Logo Scaled to 30 wide - sto.birch plywood.B/carveToadstoolStoLowerLeft.gcode", "w") as gcodeFile:
         gcodeFile.write(gcodeOutputLines)
+
+
+def cutToadstoolStoLowerLeft():
+    lowerLeftStoConventionalInsideGcodeFilename = "/home/oy753c/desktops/toadstool/carveco/Toolpaths/Toadstool Logo Scaled to 30 wide - sto.birch plywood.B/lower left sto conventional inside.gcode"
+    with open(lowerLeftStoConventionalInsideGcodeFilename, "r") as gcodeFile:
+        lowerLeftStoConventionalInsideGcode = gcodeFile.read()
+
+    lowerLeftStoClimbInsideGcodeFilename = "/home/oy753c/desktops/toadstool/carveco/Toolpaths/Toadstool Logo Scaled to 30 wide - sto.birch plywood.B/lower left sto climb inside.gcode"
+    with open(lowerLeftStoClimbInsideGcodeFilename, "r") as gcodeFile:
+        lowerLeftStoClimbInsideGcode = gcodeFile.read()
+
+    lowerLeftStoConventionalOutsideGcodeFilename = "/home/oy753c/desktops/toadstool/carveco/Toolpaths/Toadstool Logo Scaled to 30 wide - sto.birch plywood.B/lower left sto conventional outside.gcode"
+    with open(lowerLeftStoConventionalOutsideGcodeFilename, "r") as gcodeFile:
+        lowerLeftStoConventionalOutsideGcode = gcodeFile.read()
+
+    lowerLeftStoClimbOutsideGcodeFilename = "/home/oy753c/desktops/toadstool/carveco/Toolpaths/Toadstool Logo Scaled to 30 wide - sto.birch plywood.B/lower left sto climb outside.gcode"
+    with open(lowerLeftStoClimbOutsideGcodeFilename, "r") as gcodeFile:
+        lowerLeftStoClimbOutsideGcode = gcodeFile.read()
+
+    gcodeOutputLines = "T1\n"           # Tool 1
+    gcodeOutputLines += "G17\n"         # Draw Arcs in the XY plane, default.
+    gcodeOutputLines += "G21\n"         # All distances and positions are in mm
+    gcodeOutputLines += "G90\n"         # All distances and positions are Absolute values from the current origin.
+    gcodeOutputLines += "G0Z5.0000\n"   # A Rapid positioning move at the Rapid Feed Rate to Z5.0
+    gcodeOutputLines += "S10000M3\n"    # Set Spindle speed in RPM. 10,000 RPM.  Then turn the motor on.
+
+    gcodeOutputLines += "; Inside\n"
+    gcodeOutputLines += transformGcode(lowerLeftStoConventionalInsideGcode, 0.0, 0.0, -5.5, 100.0, 400.0)
+    gcodeOutputLines += transformGcode(lowerLeftStoConventionalInsideGcode, 0.0, 0.0, -7.5, 100.0, 200.0)
+    gcodeOutputLines += transformGcode(lowerLeftStoConventionalInsideGcode, 0.0, 0.0, -9.5, 100.0, 200.0)
+    gcodeOutputLines += transformGcode(lowerLeftStoConventionalInsideGcode, 0.0, 0.0, -11.5, 100.0, 200.0)
+    gcodeOutputLines += transformGcode(lowerLeftStoConventionalInsideGcode, 0.0, 0.0, -12.5, 100.0, 200.0)
+    #  Do these two in the opposite direction to raise grain and then trim it off.
+    gcodeOutputLines += transformGcode(lowerLeftStoClimbInsideGcode, 0.0, 0.0, -12.5, 100.0, 200.0)
+    gcodeOutputLines += transformGcode(lowerLeftStoConventionalInsideGcode, 0.0, 0.0, -12.5, 100.0, 200.0)
+
+    gcodeOutputLines += "G0Z5.0000\n"   # A Rapid positioning move at the Rapid Feed Rate to Z5.0
+
+    gcodeOutputLines += "; Outside\n"
+    gcodeOutputLines += transformGcode(lowerLeftStoConventionalOutsideGcode, 0.0, 0.0, -5.5, 100.0, 400.0)
+    gcodeOutputLines += transformGcode(lowerLeftStoConventionalOutsideGcode, 0.0, 0.0, -7.5, 100.0, 200.0)
+    gcodeOutputLines += transformGcode(lowerLeftStoConventionalOutsideGcode, 0.0, 0.0, -9.5, 100.0, 200.0)
+    gcodeOutputLines += transformGcode(lowerLeftStoConventionalOutsideGcode, 0.0, 0.0, -11.5, 100.0, 200.0)
+    gcodeOutputLines += transformGcode(lowerLeftStoConventionalOutsideGcode, 0.0, 0.0, -12.5, 100.0, 200.0)
+    #  Do these two in the opposite direction to raise grain and then trim it off.
+    gcodeOutputLines += transformGcode(lowerLeftStoClimbOutsideGcode, 0.0, 0.0, -12.5, 100.0, 200.0)
+    gcodeOutputLines += transformGcode(lowerLeftStoConventionalOutsideGcode, 0.0, 0.0, -12.5, 100.0, 200.0)
+
+    gcodeOutputLines += "M5\n"                  # Turn spindle motor off
+    gcodeOutputLines += "G0Z5.0000\n"           # Rapidly raise the cutting head above the workpiece
+    gcodeOutputLines += "G0X0.0000Y290.0000\n"  # Rapidly move the cutting head to top left out-of-the-way position
+    gcodeOutputLines += "M2\n"                  # End program.
+
+    with open("/home/oy753c/desktops/toadstool/carveco/Toolpaths/Toadstool Logo Scaled to 30 wide - sto.birch plywood.B/cutToadstoolStoLowerLeft.gcode", "w") as gcodeFile:
+        gcodeFile.write(gcodeOutputLines)
+
 
 
 if __name__ == '__main__':
